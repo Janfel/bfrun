6c3c1510ecd270c99c5a47c4cdf7a4d5b830d979
Jan Felix Langenbach <o.hase3@gmail.com>
2019-03-12 17:16:37 +0100

    Introduce lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2019 Jan Felix Langenbach
  *
- * This file is part of src.
+ * This file is part of bfrun.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,9 +16,3 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http: //www.gnu.org/licenses/>.
  */
-
-extern crate bfrun;
-
-fn main() {
-    println!("Hello, world!");
-}
