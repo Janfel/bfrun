adde49c30cecec2811f3667bdcf49c7525a9c697
Jan Felix Langenbach <o.hase3@gmail.com>
2019-03-12 18:45:42 +0100

    Fix license header in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2019 Jan Felix Langenbach
  *
- * This file is part of src.
+ * This file is part of bfrun.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
