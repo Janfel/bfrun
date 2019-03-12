004c1d6b56179579bdd9745aa29f55869cef1529
Jan Felix Langenbach <o.hase3@gmail.com>
2019-03-12 18:47:03 +0100

    Introduce run_file() and run_stream().

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,42 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http: //www.gnu.org/licenses/>.
  */
+
+use std::io::{Read, Write};
+
+/// Interprets a brainfuck file.
+///
+/// This function takes an input stream and treats it
+/// as a brainfuck file. Does more optimizations than
+/// `run_stream`, but blocks when encountering a loop.
+///
+/// # Arguments
+///
+/// * `input` - A reader containing the brainfuck file.
+/// * `bfin` - A reader corresponding to the brainfuck program's stdin.
+/// * `bfout` - A reader corresponding to the brainfuck programs stdout.
+pub fn run_file<R, W>(input: R, bfin: R, bfout: W)
+where
+    R: Read,
+    W: Write,
+{
+    unimplemented!();
+}
+/// Interprets a brainfuck stream.
+///
+/// This function takes an input stream of brainfuck instructions and executes
+/// them. Does less optimizations than `run_file`, but does not block when
+/// encountering a loop.
+///
+/// # Arguments
+///
+/// * `input` - A reader corresponding to the brainfuck stream.
+/// * `bfin` - A reader corresponding to the brainfuck program's stdin.
+/// * `bfout` - A reader corresponding to the brainfuck programs stdout.
+pub fn run_stream<R, W>(input: R, bfin: R, bfout: W)
+where
+    R: Read,
+    W: Write,
+{
+    unimplemented!();
+}
